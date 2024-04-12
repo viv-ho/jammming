47fb4313c484644dd955b8a6ced2d1fca82eae0c
Vivian Ho <vivianho711@hotmail.com>
2024-04-12 11:18:23 +0100

    SearchBar added

---
+++ src/SearchBar.js
@@ -0,0 +1,12 @@
+import './SearchBar.css'
+
+function SearchBar() {
+    return (
+        <div className='SearchBar'>
+            <input className='input'></input>
+            <button className='button'>Search</button>
+        </div>
+    )
+}
+
+export default SearchBar
